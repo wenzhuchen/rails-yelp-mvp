dd4c518d2a1c31d7b11a2e2833e7230f2fe6ac1b
wenzhu chen <wenzhu.chen1@gmail.com>
2017-09-30 17:33:40 +0200

    added style for home page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,7 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @review = Review.new
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants do
-    resources :reviews, only: [ :new, :create ]
+    resources :reviews, only: [ :create ]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
